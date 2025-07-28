38958246664f5cc65c8b8e8e1976e84e462e2523
Jatin Yadav <jatinyadav17716@gmail.com>
2025-07-28 10:18:51 +0530

    Initial commit

---
+++ Day1/note.js
@@ -0,0 +1,17 @@
+// single page application --> website that do not reload on performing action
+// Multi page application --> website that do not reload on performing action
+
+// library --> gives you the boilerplate of your application the other thing you need you can install them 
+
+// framework --> gives you the boilerplate with the services like -- testing, state management, routing those things which are required to build an application -- all code by default
+
+// react is a library created by meta
+
+// angular is a frontend framework created by google
+
+// nextjs is the fullstack framework of react
+
+// reconcilation --> react ki vo capability jiski vajah se pura page refresh na hokar jo update hua hai element sirf usme hi changes aate hai   --    vo virtual dom bna leta hai jbb changes hote hai toh phir se ekk new virtual dom bnata hai purane vle se compare krna hai aur purane vle ko delete krr deta hai aur jo changes hai vo add krr deta hai bina page refresh kre
+
+
+//jsx --> combination of js and html
