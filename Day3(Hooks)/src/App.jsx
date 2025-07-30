c2d3613a70daa006a25f7579dd0a32bf0d692371
Jatin Yadav <heroraoshab2005@gmail.com>
2025-07-30 18:44:30 +0530

    hooks

--- Day3(Hooks)/src/App.jsx
+++ Day3(Hooks)/src/App.jsx
@@ -1,7 +1,24 @@
 const App = () => {
+  // js logic
+  // non paramatrized function
+  const handleclick = ()=>{
+    alert("button clicked")
+  }
+  const handleparaclick = (para)=>{
+    alert(para)
+  }
+  
   return (
+    <>
     <div>App</div>
+    <div>Hello</div>
+    <button onClick={handleclick}>click</button>
+    <button onClick={()=>handleparaclick("hlo bhai log")}>click(para)</button>
+    </>
   )
 }
 
 export default App
+
+
+// a function component will always return html
